e660500292728f716fdf94574e0e650bb4e2903c
alexey.savin <alexey.savin@taskdata.ru>
2019-04-11 13:31:50 +0300

    added UDF

--- src/main/kotlin/ru/taskdata/datamart/Hello.kt
+++ src/main/kotlin/ru/taskdata/datamart/Hello.kt
@@ -2,10 +2,26 @@ package ru.taskdata.datamart
 
 import org.apache.spark.SparkConf
 import org.apache.spark.api.java.JavaSparkContext
+import org.apache.spark.sql.Row
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.api.java.UDF1
 import org.elasticsearch.hadoop.cfg.ConfigurationOptions
 import org.elasticsearch.spark.sql.api.java.JavaEsSparkSQL
+import java.security.Timestamp
 
+private val latestCost = UDF1<Array<Row>, Long> { costStructure ->
+    if (costStructure != null) {
+        costStructure
+            .maxBy {
+                val applicationDate = it.getAs<Timestamp>("application_date")
+                val timestamp =
+                    if (applicationDate != null) applicationDate else it.getAs<Timestamp>("approvement_date")
+                timestamp.timestamp
+            }?.getAs<Long>("value")
+    } else {
+        0
+    }
+}
 
 fun main() {
 
